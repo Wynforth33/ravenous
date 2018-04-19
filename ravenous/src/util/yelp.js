64dd8bc59de52f33da2f7e8d18f7b9d0af263f1f
Wynforth <jkethethird@gmail.com>
2018-04-18 23:43:28 -0500

    Published to Surge at wyn_ravenous.surge.sh

--- ravenous/src/util/yelp.js
+++ ravenous/src/util/yelp.js
@@ -1,16 +1,18 @@
 // Yelp API Key
 const apiKey = '4kmFyBBpdnMCBgL_kK9Txbn3G4oIyvzhm2LgApX_pr8KdNsc6lMlQhGaypboYWAzqO6iYJkI4fcxC4E4x1pcDRYMl3hERqJtYqwevEYQoJak6pqM6cj-30-5mNzHWnYx';
 
-// Yelp API Object - Handles the Yelp Search Object/Function 
+// Yelp API Object - Handles the Yelp Search Object/Function
 const Yelp = {
   search(term, location, sortBy){
     return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-      headers: { Authorization: `Bearer ${apiKey}`}
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
     }).then(response => {
       return response.json();
     }).then(jsonResponse => {
-      if (jsonResponse.businesses){
-        return jsonResponse.businesses.map(business => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
           id: business.id,
           imageSrc: business.image_url,
           name: business.name,
@@ -21,7 +23,7 @@ const Yelp = {
           category: business.categories[0].title,
           rating: business.rating,
           reviewCount: business.review_count
-        });
+        }));
       }
     });
   }
